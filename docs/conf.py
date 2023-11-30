f59195d15ccdae711e59f10eeafd3f4809df1596
Bas Altena <b.altena@uu.nl>
2023-11-30 15:58:44 +0100

    created other make file

--- docs/conf.py
+++ docs/conf.py
@@ -59,6 +59,8 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 #
 html_theme = 'pydata_sphinx_theme'
 
+numpydoc_class_members_toctree = False
+
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
