635b6a43acae5b8e0828570b9728e28060503a9f
Bas Altena <b.altena@uu.nl>
2024-02-01 09:45:47 +0100

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -53,7 +53,7 @@ nbsphinx_thumbnails = {
     'Diagnostics/mediod_method01': '_static/lagrangian-diag-logo.png',
 }
 
-numpydoc_show_class_members = False
+# numpydoc_show_class_members = False
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
