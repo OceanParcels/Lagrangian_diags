36cc77691c66ae37058e3450eb4bfaa0bcdf31ca
Vecko <36369090+VeckoTheGecko@users.noreply.github.com>
2025-09-21 16:52:20 +0200

    Remove 'saving of output' from kde_parcels
    Saving code wasn't working properly

--- Diagnostics/Functions/GKDE_01.py
+++ Diagnostics/Functions/GKDE_01.py
@@ -1,16 +1,18 @@
 import numpy as np
 from scipy.stats import gaussian_kde
 
+
 def rem_nans(ds):
     """
     This renders lon and lat variables without nans for the last timestep.
     """
-    bad_indices = np.isnan(ds['lon'][:,-1]) | np.isnan(ds['lat'][:,-1])
+    bad_indices = np.isnan(ds["lon"][:, -1]) | np.isnan(ds["lat"][:, -1])
     good_indices = ~bad_indices
-    lon_end_nonans = ds['lon'][:,-1][good_indices]
-    lat_end_nonans = ds['lat'][:,-1][good_indices]
+    lon_end_nonans = ds["lon"][:, -1][good_indices]
+    lat_end_nonans = ds["lat"][:, -1][good_indices]
 
     return lon_end_nonans, lat_end_nonans
+
 
 def kde_vals(lon_end_nonans, lat_end_nonans):
     """
@@ -36,13 +38,11 @@ def kde_vals(lon_end_nonans, lat_end_nonans):
 
     return x, y, z
 
-def kde_parcels(ds, nsavedir=None):
+
+def kde_parcels(ds):
     """
     Inputs
     ds : xarray dataset of the parcels output netcdf file
-    nsavedir: directory where to save the outputs. By default it is None, and will not save outputs.
-    If outputs want to be saved specify directory,
-    for example: '/data/oceanparcels/output_data/data_LauraGN/outputs_parcels/kde_calcs/'
 
     Outputs
     KDE for the las timestep fields of lon and lat
@@ -51,10 +51,4 @@ def kde_parcels(ds, nsavedir=None):
     # Remove nans:
     lon_end_nonans, lat_end_nonans = rem_nans(ds)
 
-    kde_x, kde_y, kde_z = kde_vals(lon_end_nonans, lat_end_nonans)
-
-    if nsavedir:
-        savename = savedir + 'KDE_' + nfile.split('/')[-1].split('.nc')[0] + '.npz'
-        np.savez(savename, kde_x=kde_x, kde_y=kde_y, kde_z=kde_z)
-
-    return kde_x, kde_y, kde_z
+    return kde_vals(lon_end_nonans, lat_end_nonans)
